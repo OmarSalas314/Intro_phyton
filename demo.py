80ab333f1ac1912a1502a2151bf1f2f6508b9112
OmarSalas314 <omarsalasvieyra@gmail.com>
2022-07-26 16:53:41 -0600

    Creando repo del curso de phyton de microsoft en youtube

---
+++ demo.py
@@ -0,0 +1,13 @@
+#first_name = 'Christopher'
+#last_name = 'Harrison'
+
+#first_name = input('Please enter your first name: ')
+#last_name = input('Please enter your last name: ')
+#print(first_name + last_name)
+#print('Hello, ' + first_name.capitalize() + ' ' + last_name.capitalize())
+
+first_name = 'Christopher'
+last_name = 'Harrison'
+
+output = f'Hello, {first_name} {last_name}'
+print(output)
---
+++ numbers.py
@@ -0,0 +1,12 @@
+# pi= 3.14159
+# print(pi)
+
+# first_num = 5
+# second_num = 6
+# print(first_num + second_num)
+
+# Cuando metes un input debes convertir a int o float para no generar error
+first_num = input('Please enter a first number')
+second_num = input('Please enter a second number')
+print(int(first_num) + int(second_num))
+
